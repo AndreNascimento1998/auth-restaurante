9ffae9aa77ea419dbdf158513d5e2edba1e2f513
André Joarez <andre.ncardoso@hotmail.com>
2023-07-11 19:06:41 -0300

    chore: update

--- src/infra/database/mongo.module.ts
+++ src/infra/database/mongo.module.ts
@@ -4,6 +4,7 @@ import { AuthSchemas } from 'src/infra/database/schemas/auth.schemas';
 
 const deps = [
     
+    //MongooseModule.forRoot('mongodb+srv://andrejoarez428:wYl6HfkkVnI3kByh@restaurante-api.pmpxvgn.mongodb.net/restaurante-auth'),
     MongooseModule.forRoot('mongodb+srv://andrejoarez428:wYl6HfkkVnI3kByh@restaurante-api.pmpxvgn.mongodb.net/restaurante-auth'),
     
     MongooseModule.forFeature([
